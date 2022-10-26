47353d91535465194fba8045737f744daa55a513
JNKarif <arif.rahman041@gmail.com>
2022-10-26 21:42:46 +0600

    course-details-added

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express= require('express')
 const app = express();
+const cors= require('cors')
 const port =process.env.PORT || 5000;
+
+app.use(cors());
+
+const category= require('./data/category.json')
+const courses= require('./data/course.json')
 
 app.get('/',(req,res)=>{
     res.send('Courses API Running')
+})
+
+app.get('/category',(req, res)=>{
+    res.send(category)
+})
+
+app.get('/courses',(req,res)=>{
+    res.send(courses)
 })
 
 app.listen(port,()=>{
