995e0018c42e617ee782d8bdcea81fdb76e54317
JNKarif <arif.rahman041@gmail.com>
2022-10-27 17:16:33 +0600

    premium-id-added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(cors());
 
 const category = require('./data/category.json')
 const courses = require('./data/course.json')
+const premium = require('./data/premium.json')
 
 app.get('/', (req, res) => {
     res.send('Courses API Running')
@@ -24,6 +25,13 @@ app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const selectedCourse = courses.find(c =>c._id===id);
     res.send(selectedCourse)
+})
+
+
+app.get('/premium/:id', (req, res)=>{
+    const id= req.params.id;
+    const selectedPremium= premium.find(p=>p._id===id);
+    res.send(selectedPremium)
 })
 
 app.listen(port, () => {
